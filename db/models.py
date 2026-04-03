e84c83c88682ca27e535a0538d208a4f8abdbf1c
gelexxi <schew4uk98@gmail.com>
2026-04-03 22:58:57 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -14,7 +14,8 @@ class Guild(models.Model):
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE,
+                             related_name="skills")
 
 
 class Player(models.Model):
